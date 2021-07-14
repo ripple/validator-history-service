69155d2711f1d797140a40aae8e257439824852c
mbhandary <mayurbhandary96@gmail.com>
2021-07-14 18:50:10 -0400

    only counts distinct ips

--- src/api/routes/v1/health.ts
+++ src/api/routes/v1/health.ts
@@ -4,9 +4,8 @@ import { query } from '../../../shared/database'
 export default async function handleHealth(_u: Request, res: Response):Promise<void>{
     try {
         const count = await query('crawls')
-            .count('*')
+            .countDistinct('ip')
             .where('connected', '=', true)
-
         res.status(200).send(count[0])
     } catch {
         res.send({ result: 'error', message: 'internal error' })
