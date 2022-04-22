8e04f76ea8674fdd191f47c847ec4ab312b639ad
Mayukha Vadari <mvadari@ripple.com>
2022-04-22 12:03:20 -0400

    create new API URLs

--- src/api/routes/v1/index.ts
+++ src/api/routes/v1/index.ts
@@ -13,17 +13,12 @@ api.use('/health', handleHealth)
 api.use('/network/validator_reports', handleDailyScores)
 api.use('/network/topology', handleTopology)
 
-api.use('/network/topology/nodes/:publicKey', handleNode)
-// ^ This will be replaced - copied for easier migration
 api.use('/network/topology/nodes', handleNodes)
+api.use('/network/topology/nodes/:network', handleNodes)
 api.use('/network/topology/node/:publicKey', handleNode)
 
-api.use('/network/validators/:publicKey/reports', handleValidatorReport)
-api.use('/network/validators/:publicKey/manifests', handleValidatorManifest)
-api.use('/network/validators/:publicKey', handleValidator)
-// ^ These will be replaced - copied for easier migration
 api.use('/network/validators', handleValidators)
-
+api.use('/network/validators/:network', handleValidators)
 api.use('/network/validator/:publicKey', handleValidator)
 api.use('/network/validator/:publicKey/reports', handleValidatorReport)
 api.use('/network/validator/:publicKey/manifests', handleValidatorManifest)
