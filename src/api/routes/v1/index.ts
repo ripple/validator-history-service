0774822bc27894e3b61807115d4331c3a5feb977
Mayukha Vadari <mvadari@ripple.com>
2022-04-22 12:02:18 -0400

    copy API calls

--- src/api/routes/v1/index.ts
+++ src/api/routes/v1/index.ts
@@ -8,17 +8,23 @@ import { handleValidator, handleValidators } from './validator'
 import handleValidatorReport from './validator-report'
 
 const api = createRouter()
+
 api.use('/health', handleHealth)
-
-api.use('/network/topology', handleTopology)
-api.use('/network/topology/nodes', handleNodes)
-api.use('/network/topology/nodes/:publicKey', handleNode)
-
-api.use('/network/validators', handleValidators)
-api.use('/network/validators/:publicKey', handleValidator)
-api.use('/network/validators/:publicKey/manifests', handleValidatorManifest)
-api.use('/network/validators/:publicKey/reports', handleValidatorReport)
-
 api.use('/network/validator_reports', handleDailyScores)
+api.use('/network/topology', handleTopology)
+
+api.use('/network/topology/nodes/:publicKey', handleNode)
+api.use('/network/topology/nodes', handleNodes)
+// ^ This will be replaced - copied for easier migration
+api.use('/network/topology/node/:publicKey', handleNode)
+
+api.use('/network/validators/:publicKey/reports', handleValidatorReport)
+api.use('/network/validators/:publicKey/manifests', handleValidatorManifest)
+api.use('/network/validators/:publicKey', handleValidator)
+api.use('/network/validators', handleValidators)
+// ^ These will be replaced - copied for easier migration
+api.use('/network/validator/:publicKey', handleValidator)
+api.use('/network/validator/:publicKey/reports', handleValidatorReport)
+api.use('/network/validator/:publicKey/manifests', handleValidatorManifest)
 
 export default api
