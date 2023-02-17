59724001f47619443c9a2b7c2b4491ee35b5a105
Phu Pham <ppham@ripple.com>
2023-02-17 17:12:08 -0500

    fix nft validators

--- src/shared/database/setup.ts
+++ src/shared/database/setup.ts
@@ -125,6 +125,10 @@ async function setupValidatorsTable(): Promise<void> {
       table.string('networks').after('chain')
     })
   }
+  // Remove nft-dev validators, since they have been decomissioned.
+  await db().schema.raw(
+    "UPDATE validators SET chain = 'nft-dev', networks = 'nft-dev' WHERE unl LIKE '%nftvalidators.s3.us-west-2.amazonaws.com%'",
+  )
 }
 
 async function setupHourlyAgreementTable(): Promise<void> {
