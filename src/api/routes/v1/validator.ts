7b52360c6bb951b4dbf2af27e45fac66d0684e27
Phu Pham <ppham@ripple.com>
2023-10-16 18:19:13 -0400

    amendments returns array instead of string

--- src/api/routes/v1/validator.ts
+++ src/api/routes/v1/validator.ts
@@ -42,7 +42,7 @@ interface ValidatorResponse {
   } | null
   partial: boolean
   unl: boolean
-  amendments?: string | Array<{ id: string; name: string }>
+  amendments: Array<{ id: string; name: string }> | null
   base_fee?: number
   reserve_base?: number
   reserve_inc?: number
@@ -121,7 +121,7 @@ async function getValidators(): Promise<ValidatorResponse[]> {
     ])
     .where('validators.revoked', '=', 'false')
     .orderBy(['validators.master_key', 'validators.signing_key'])
-    .then((res: dbResponse[]) => res.map(formatResponse))
+    .then(async (res: dbResponse[]) => Promise.all(res.map(formatResponse)))
 }
 
 /**
@@ -145,11 +145,13 @@ async function cacheValidators(): Promise<void> {
  * @param resp - Database response.
  * @returns Validator in correct response format.
  */
-function formatResponse(resp: dbResponse): ValidatorResponse {
-  const { agreement_1hour, agreement_24hour, agreement_30day } = resp
+async function formatResponse(resp: dbResponse): Promise<ValidatorResponse> {
+  const { agreement_1hour, agreement_24hour, agreement_30day, amendments } =
+    resp
   let hour1_score = null
   let hour24_score = null
   let day30_score = null
+  let amendments_list = null
 
   if (agreement_1hour !== null) {
     hour1_score = formatAgreementScore(agreement_1hour)
@@ -161,6 +163,10 @@ function formatResponse(resp: dbResponse): ValidatorResponse {
 
   if (agreement_30day !== null) {
     day30_score = formatAgreementScore(agreement_30day)
+  }
+
+  if (amendments) {
+    amendments_list = await formatAmendments(amendments)
   }
 
   return {
@@ -178,7 +184,7 @@ function formatResponse(resp: dbResponse): ValidatorResponse {
     partial: resp.partial,
     unl: resp.unl ?? false,
     revoked: resp.revoked,
-    amendments: resp.amendments,
+    amendments: amendments_list,
     base_fee: resp.base_fee,
     reserve_base: resp.reserve_base,
     reserve_inc: resp.reserve_inc,
@@ -256,10 +262,6 @@ export async function handleValidator(
     if (validator === undefined) {
       res.send({ result: 'error', message: 'validator not found' })
       return
-    }
-
-    if (validator.amendments && typeof validator.amendments === 'string') {
-      validator.amendments = await formatAmendments(validator.amendments)
     }
 
     res.send({ ...validator, result: 'success' })
