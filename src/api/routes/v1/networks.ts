53afdc5e64338e40689e4dd2f44af7633824dd53
Phu Pham <ppham@ripple.com>
2024-03-07 11:53:12 -0500

    fix lint

--- src/api/routes/v1/networks.ts
+++ src/api/routes/v1/networks.ts
@@ -4,21 +4,25 @@ import { getNetworks } from '../../../shared/database'
 import { Network } from '../../../shared/database/networks'
 
 /**
- * Handle networks request
+ * Handle networks request.
  *
  * @param _u - Unused express request.
  * @param res - Express response.
+ * @returns Void.
  */
-export default async function handleNetworks(_u: Request, res: Response) {
+export default async function handleNetworks(
+  _u: Request,
+  res: Response,
+): Promise<void> {
   try {
     const networks: Network[] = await getNetworks()
     const response = {
       result: 'success',
       count: networks.length,
-      networks: networks,
+      networks,
     }
     res.send(response)
   } catch (err) {
-    res.status(500).send({ result: 'error', message: 'internal error' })
+    res.status(500).send({ result: 'error', message: err })
   }
 }
