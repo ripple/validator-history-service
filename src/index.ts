18535a60b377347c3566de54a55fc6650e8ed821
Raj Patel <rajp@ripple.com>
2025-08-20 10:35:37 -0400

    revert change to try another approach

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@
 /* eslint-disable import/unambiguous -- main file */
 /* eslint-disable global-require, node/global-require -- main file */
 /* eslint-disable @typescript-eslint/no-require-imports -- main file */
+/* eslint-disable no-console -- for testing only */
+console.log(`Debug node args: ${process.execArgv.join(', ')}`)
+console.log(`Debug process args: ${process.argv.join(', ')}`)
 if (process.argv.includes('--api')) {
   require('./api')
 } else if (process.argv.includes('--connections')) {
