acc2649425e566886504bb6eba6db37cc2e21f92
Raj Patel <rajp@ripple.com>
2025-09-11 16:37:44 -0400

    update @xrplf/eslint-config to 2.0.0

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 const globals = require('globals')
-const eslintConfig = require('@patelraj96/eslint-config/base')
+const eslintConfig = require('@xrplf/eslint-config/base')
 const tseslint = require('typescript-eslint')
 const jest = require('eslint-plugin-jest')
 
