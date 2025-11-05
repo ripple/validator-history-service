b0c9de2ac417038efa830094d29c6e39a2170529
Chenna Keshava B S <ckeshavabs@gmail.com>
2025-11-05 18:19:20 +0530

    feat: remove stale WS connections from the connection_health table

--- src/connection-manager/connections.ts
+++ src/connection-manager/connections.ts
@@ -37,7 +37,8 @@ const REPORTING_INTERVAL = 15 * 60 * 1000
 const BACKTRACK_INTERVAL = 30 * 60 * 1000
 const BASE_RETRY_DELAY = 1 * 1000
 const MAX_RETRY_DELAY = 30 * 1000
-const WS_HEARTBEAT_INTERVAL = 10 * 60 * 1000
+const WS_HEARTBEAT_CHECK_INTERVAL = 1 * 60 * 1000
+const WS_HEARTBEAT_TIMEOUT = 60 * 1000
 
 // The frequent closing codes seen so far after connections established include:
 //  1008: Policy error: client is too slow. (Most frequent)
@@ -143,13 +144,17 @@ setInterval(() => {
   let staleConnections = 0
   // report the stale connections whose heartbeat is older than 60 seconds (missing 15 validated ledgers)
   for (const [url, heartbeat] of webSocketHeartbeat.entries()) {
-    if (Date.now() - heartbeat.getTime() > 60 * 1000) {
-      log.error('WS ERROR: stale connection for: ' + url + ' last received at: ' + heartbeat.toISOString() + '. (' + (Date.now() - heartbeat.getTime())/1000 + 'seconds ago)')
+    if (Date.now() - heartbeat.getTime() > WS_HEARTBEAT_TIMEOUT) {
+      log.error('WS ERROR: stale connection for: ' + url + ' last receiveda message at: ' + heartbeat.toISOString() + '. (' + (Date.now() - heartbeat.getTime())/1000 + 'seconds ago). Terminating this WS connection.')
       staleConnections++
+
+      // Note: It is possible that the Javascript runtime never garbage-collects these stale websockets. As part of future work, we should also record the `ws` instance and terminate stale web-sockets.
+      void updateConnectionHealthStatus(url, false)
+      webSocketHeartbeat.delete(url)
     }
   }
   log.error('WS ERROR: Total number of stale connections: ' + staleConnections)
-}, WS_HEARTBEAT_INTERVAL)
+}, WS_HEARTBEAT_CHECK_INTERVAL)
 
 /**
  * Tries to find a valid WebSockets endpoint for a node.
